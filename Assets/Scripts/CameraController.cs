4e653698f4e0700c33fb7fa553ae8f53adc8e34f
CorPruijs <cor@pruys.com>
2015-07-16 22:03:01 +0200

    Added text objects that display coords, added a script that lets the cammera smoothly follow the turret

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform turretCameraPosition;
+	public Transform pipeEnd;
+
+	public float moveSpeed = 3;
+
+	private float startTime;
+	private float journeyLength;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPos = turretCameraPosition.position;
+		Vector3 currentCameraPos = transform.position;
+
+
+		transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed);
+		transform.LookAt(pipeEnd.position);
+	
+	}
+}
---
+++ Assets/Scripts/CoordTextUpdate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoordTextUpdate : MonoBehaviour {
+
+	public TextMesh textMesh;
+	[MultilineAttribute] public string prependString;
+
+	// Use this for initialization
+	void Start () {
+		textMesh.text = prependString + "\nx:" + transform.position.x + " z:" + transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
