66144d0468a172e525f4993665e5514277485d48
CorPruijs <cor@pruys.com>
2015-07-16 15:50:32 +0200

    Added turret script

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : MonoBehaviour {
+
+	public GameObject turretBase;
+	public GameObject cannonBase;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		rotateTurretBase(horizontalInput);
+		rotateTurretCannon(verticalInput);
+
+	}
+
+	void rotateTurretBase(float degrees) {
+		turretBase.transform.Rotate(new Vector3(0, degrees, 0));
+	}
+
+	void rotateTurretCannon(float degrees) {
+		cannonBase.transform.Rotate(new Vector3(0, degrees, 0));
+	}
+}
